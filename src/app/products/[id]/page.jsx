1f765569d2fe360d8cd7854748c2352aac79dd76
Anawar Hossain <anwarhossainhossain01@gmail.com>
2026-04-30 22:02:37 +0600

    Update product details page to fetch and display product information dynamically

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,10 +1,25 @@
+'use client'
+
+
+import useAllProducts from '@/lib/products';
+import { useParams } from 'next/navigation';
 import React from 'react';
 
 const ProductDetailsPage = () => {
+
+    const { id } = useParams();
+    console.log(id)
+
+    const { products, loading, error } = useAllProducts();
+    
+    const product = products.find((p) => p.id === Number(id));
+    console.log(product)
+
+    
+
+
     return (
-        <div>
-            this details page
-        </div>
+      <div>Details page</div>
     );
 };
 
